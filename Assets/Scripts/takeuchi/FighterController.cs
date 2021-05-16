493cbfe53bfcb1cf361d63ee9d201f927b44c2e0
D. Suzuki <bboydaisuke@gmail.com>
2021-05-16 23:39:23 +0900

    格闘ゲームのプロトタイプを追加した

---
+++ Assets/Scripts/takeuchi/FighterController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+// Photon 用の名前空間を参照する
+using Photon.Pun;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FighterController : MonoBehaviour
+{
+    [SerializeField] float m_movePower = 5f;
+    [SerializeField] float m_jumpPower = 5f;
+    [SerializeField] float m_jumpDump = 0.8f;
+    Rigidbody2D m_rb = null;
+    float m_h = 0f;
+    bool m_isGrounded = false;
+    Animator m_anim = null;
+    float m_lastHorizontalInput = 1f;
+    PhotonView m_view = null;
+
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        m_anim = GetComponent<Animator>();
+        m_view = GetComponent<PhotonView>();
+    }
+
+    void Update()
+    {
+        if (!m_view || !m_view.IsMine) return;      // 自分が生成したものだけ処理する
+
+        m_h = Input.GetAxisRaw("Horizontal");
+
+        FlipX(m_h);
+
+        if (Input.GetButtonDown("Jump") && m_isGrounded)
+        {
+            m_rb.AddForce(Vector2.up * m_jumpPower, ForceMode2D.Impulse);
+        }
+
+        if (!Input.GetButton("Jump"))
+        {
+            Vector2 velocity = m_rb.velocity;
+            
+            if (velocity.y > 0)
+            {
+                velocity.y *= m_jumpDump;
+                m_rb.velocity = velocity;
+            }
+        }
+
+        if (m_anim)
+            m_anim.SetBool("Punch", Input.GetButtonDown("Fire1"));
+    }
+
+    void FixedUpdate()
+    {
+        if (!m_view || !m_view.IsMine) return;      // 自分が生成したものだけ処理する
+
+        m_rb.AddForce(m_movePower * m_h * Vector2.right, ForceMode2D.Force);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 手抜き接地判定。このオブジェクトにトリガーがもう一つ付いてしまうと使えない。その場合は BoxCast などを使って接地判定をする。
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            m_isGrounded = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            m_isGrounded = false;
+        }
+    }
+
+    void FlipX(float horizontalInput)
+    {
+        if (horizontalInput != 0)
+        {
+            if (horizontalInput * m_lastHorizontalInput < 0f)
+            {
+                Vector3 scale = this.transform.localScale;
+                scale.x *= -1;
+                this.transform.localScale = scale;
+            }
+
+            m_lastHorizontalInput = m_h;
+        }
+    }
+}
---
+++ Assets/Scripts/takeuchi/PunchController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+// Photon 用の名前空間を参照する
+using Photon.Pun;
+
+public class PunchController : MonoBehaviour
+{
+    [SerializeField] float m_punchPower = 5f;
+    PhotonView m_view = null;
+
+    void Start()
+    {
+        m_view = GetComponent<PhotonView>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 手抜き。ここは本来は、他プレイヤーのオーナーのViewから、自らを吹っ飛ばす関数をRPCで呼ぶべき
+        Rigidbody2D other = collision.gameObject.GetComponent<Rigidbody2D>();
+
+        if (other)
+        {
+            Vector3 dir = other.transform.position - this.transform.position;
+            other.AddForce(dir.normalized * m_punchPower, ForceMode2D.Impulse);
+        }
+    }
+}
