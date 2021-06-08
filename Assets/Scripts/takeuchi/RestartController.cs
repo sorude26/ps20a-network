b3c615809c64737cb1c284c60ea6153c32d3fe9a
K.Kobayashi <gunoka0629@gmail.com>
2021-06-08 11:46:13 +0900

    再接続機能の追加

---
+++ Assets/Scripts/takeuchi/RestartController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+[RequireComponent(typeof(NetworkGameManager))]
+public class RestartController : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+       
+    }
+
+    public void RestartSystem()
+    {
+        PhotonNetwork.LeaveRoom();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
