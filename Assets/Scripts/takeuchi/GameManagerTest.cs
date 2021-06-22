a12decc022e18069a6328dfa4ca04e6bee046c40
nogu06 <knocktokotoko@gmail.com>
2021-06-22 12:35:32 +0900

    変数名、関数名の訂正。XMLコメントを追加。

--- Assets/Scripts/takeuchi/GameManagerTest.cs
+++ Assets/Scripts/takeuchi/GameManagerTest.cs
@@ -9,7 +9,17 @@ public class GameManagerTest : MonoBehaviourPunCallbacks, IOnEventCallback
 {
     PhotonView m_view = null;
 
-    public const byte MoveUnitsToTargetPositionEventCode = 150;
+    public const byte eventCode = 150;
+
+    /// <summary>
+    /// イベントコード一覧
+    /// </summary>
+    public enum EventCodes
+    {
+        IDied = 150　//私は死んだ
+
+    }
+
 
     enum GameStatus
     {
@@ -48,15 +58,31 @@ public class GameManagerTest : MonoBehaviourPunCallbacks, IOnEventCallback
         gameManagerList.Add(gameManager_Another);
         numberOfLivingPlayer++;
     }
+    /// <summary>
+    /// 呼び出しテスト
+    /// </summary>
+    public void Test()
+    {
+        GameEvent(EventCodes.IDied);
+
+    }
 
     /// <summary>
-    /// 自身が死んだ時に、他のプレイヤーに通知する
+    /// イベントを他のユーザーに送信する関数
     /// </summary>
-    public void PlayerDied()
+    public void GameEvent(EventCodes eventCode)
     {
-        gamestatus = GameStatus.Game_Lose;
-        SendMoveUnitsToTargetPositionEvent();
-        Debug.Log("You Lose...");
+        switch (eventCode)
+        {
+            case EventCodes.IDied:
+                gamestatus = GameStatus.Game_Lose;
+                Debug.Log("You Lose...");
+                break;
+            default:
+                break;
+        }
+
+        SendEvent((byte)eventCode);
     }
 
     /// <summary>
@@ -72,24 +98,27 @@ public class GameManagerTest : MonoBehaviourPunCallbacks, IOnEventCallback
         }
     }
 
-    public void SendMoveUnitsToTargetPositionEvent()
+    /// <summary>
+    /// イベント送信処理
+    /// </summary>
+    /// <param name="eventCode"></param>
+    public void SendEvent(byte eventCode)
     {
         object[] content = new object[] { new Vector3(10.0f, 2.0f, 5.0f), 1, 2, 5, 10 }; // Array contains the target position and the IDs of the selected units
         RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.Others }; // You would have to set the Receivers to All in order to receive this event on the local client as well
-        PhotonNetwork.RaiseEvent(MoveUnitsToTargetPositionEventCode, null, raiseEventOptions, SendOptions.SendReliable);
+        PhotonNetwork.RaiseEvent(eventCode, null, raiseEventOptions, SendOptions.SendReliable);
     }
 
+    /// <summary>
+    /// イベント受信
+    /// </summary>
+    /// <param name="e">受け取るイベント</param>
     public void OnEvent(EventData e)
     {
-        if ((int)e.Code == 150)     //こっちは死んだよって言われたら
+        if ((byte)e.Code == (byte)EventCodes.IDied)     //こっちは死んだよって言われたら
         {
             AnotherPlayerDied();
         }
     }
-    /*
-     *  ◆暫定イベントコード一覧
 
-     *  ・150 = 送信したプレイヤーが死んだ
-     * 
-     */
 }
