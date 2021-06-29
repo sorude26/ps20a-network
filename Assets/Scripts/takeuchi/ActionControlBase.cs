cfacc443f266514631fd4dfac5b5677029c6a0e8
sorude26 <sorude26@gmail.com>
2021-06-29 11:39:29 +0900

    プレイヤーの入力アクションを継承先で変更できるように、入力アクションをまとめたスクリプトを追加した

---
+++ Assets/Scripts/takeuchi/ActionControlBase.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 移動以外の入力アクションをまとめた基底クラス
+/// </summary>
+public class ActionControlBase : MonoBehaviour
+{
+    [SerializeField] protected PunchController m_punch = null;
+    protected Rigidbody2D m_rb = null;
+    protected Animator m_anim = null;
+    private void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        m_anim = GetComponent<Animator>();
+    }
+    /// <summary>
+    /// ジャンプ
+    /// </summary>
+    public virtual void Jump(float power)
+    {
+        m_rb.AddForce(Vector2.up * power, ForceMode2D.Impulse);
+    }
+    /// <summary>
+    /// 弱攻撃
+    /// </summary>
+    /// <param name="power"></param>
+    public virtual void LightAttack(float power)
+    {
+        m_punch.SetPower(power);
+        if (m_anim)
+            m_anim.SetBool("Punch",true);
+    }
+    /// <summary>
+    /// 強攻撃
+    /// </summary>
+    /// <param name="power"></param>
+    public virtual void StrongAttack(float power)
+    {
+        m_punch.SetPower(power);
+        if (m_anim)
+            m_anim.SetBool("Punch", true);
+        m_rb.AddForce(Vector2.right * this.transform.localScale.x * power, ForceMode2D.Impulse);
+    }
+    /// <summary>
+    /// 被ダメージ
+    /// </summary>
+    /// <param name="attackVector"></param>
+    public virtual void Hit(Vector3 attackVector)
+    {
+        m_rb.AddForce(attackVector, ForceMode2D.Impulse);
+    }
+}
