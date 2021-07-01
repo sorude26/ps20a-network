45d0a23b084eac7d6d8f0f7d80e412ce03e50957
nogu06 <knocktokotoko@gmail.com>
2021-07-01 17:19:27 +0900

    当たり判定のチェックを行った。当たると少し動く

--- Assets/Scripts/takeuchi/Stone.cs
+++ Assets/Scripts/takeuchi/Stone.cs
@@ -5,16 +5,36 @@ using Photon.Pun;
 
 public class Stone : MonoBehaviourPunCallbacks
 {
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
 
+    float m_punchPower = 5f;
+    PhotonView m_view = null;
+
+    void Start()
+    {
+        m_view = GetComponent<PhotonView>();
+    }
+    public void SetPower(float power)
+    {
+        m_punchPower = power;
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (m_view && m_view.IsMine && collision.gameObject.CompareTag("Player"))
+        {
+            PhotonView view = collision.gameObject.GetComponent<PhotonView>();
+
+            if (view)
+            {
+                Vector3 dir = view.transform.position - this.transform.position;
+                view.RPC("Hit", RpcTarget.Others, dir.normalized * m_punchPower);
+            }
         }
+
         if (collision.tag == "GameZoon")
         {
             Destroy(this.gameObject);
             Destroy(this);
         }
     }
+
 }
