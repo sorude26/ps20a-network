26b5985788f25c69f2f1d3125c27a9d4f639cca7
D-Itsuki <raito.kaminari@gmail.com>
2021-08-31 11:08:10 +0900

    キャラクターを選択すると、staticなm_playerPrefabNameの中身が変わるようにした

---
+++ Assets/Scripts/takeuchi/CharacterManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+// Photon 用の名前空間を参照する
+using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class CharacterManager : MonoBehaviourPunCallbacks // Photon Realtime 用のクラスを継承する
+{
+    /// <summary>プレイヤーのプレハブの名前</summary>
+    [SerializeField] public static string m_playerPrefabName = "Prefab";
+
+    public static void SetPrefabName(string name)
+    {
+        m_playerPrefabName = name;
+    }
+}
---
+++ Assets/Scripts/takeuchi/CharacterSelect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+
+    public void Select(string characterName)
+    {
+        CharacterManager.SetPrefabName(characterName);
+        Debug.Log(CharacterManager.m_playerPrefabName);
+    }
+}
